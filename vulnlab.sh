d9b900f64db2b87fce841d59f15b72162bbdf838
achuachu727 <96119005+achuachu727@users.noreply.github.com>
2023-05-25 12:58:41 +0530

    Create vulnlab.sh

---
+++ vulnlab.sh
@@ -0,0 +1,4 @@
+ docker run -itd --name vulnlab1 -d -p 5001:80 yavuzlar/vulnlab:latest
+ docker run -itd --name vulnlab2 -d -p 5002:80 yavuzlar/vulnlab:latest
+docker run -itd  --name juiceshop1 -p 3001:3000 bkimminich/juice-shop
+docker run -itd  --name juiceshop1 -p 3002:3000 bkimminich/juice-shop
